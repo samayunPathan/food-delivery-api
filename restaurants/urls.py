15fd81a48f3a168837a54d98db4ff8bcb005011c
unknown <samayunp@gmail.com>
2024-09-06 01:05:57 +0600

    basic func done

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('restaurants/', views.RestaurantListCreateView.as_view(), name='restaurant-list-create'),
+    path('restaurants/', views.RestaurantListCreateView.as_view(),
+         name='restaurant-list-create'),
     path('menus/', views.MenuListCreateView.as_view(), name='menu-list-create'),
-    # Add URLs for other views (Category, MenuItem, Modifier)
+    path('categories/', views.CategoryListCreateView.as_view(),
+         name='category-list-create'),
+    path('menu-items/', views.MenuItemListCreateView.as_view(),
+         name='menu-item-list-create'),
+    path('modifiers/', views.ModifierListCreateView.as_view(),
+         name='modifier-list-create'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Restaurant, Menu, Category, MenuItem, Modifier
 from .serializers import RestaurantSerializer, MenuSerializer, CategorySerializer, MenuItemSerializer, ModifierSerializer
@@ -8,10 +7,6 @@ from users.permissions import IsOwnerOrEmployee
 
 
 class RestaurantListCreateView(generics.ListCreateAPIView):
-    """
-    API endpoint for listing and creating restaurants.
-    Only owners can create new restaurants.
-    """
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrEmployee]
@@ -21,10 +16,6 @@ class RestaurantListCreateView(generics.ListCreateAPIView):
 
 
 class MenuListCreateView(generics.ListCreateAPIView):
-    """
-    API endpoint for listing and creating menus.
-    Only owners and employees can create new menus.
-    """
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrEmployee]
@@ -34,7 +25,43 @@ class MenuListCreateView(generics.ListCreateAPIView):
         if self.request.user.is_owner or self.request.user.is_employee and self.request.user.restaurant == restaurant:
             serializer.save()
         else:
-            raise permissions.DjangoPermissionDenied
+            raise permissions.PermissionDenied
 
 
-# Similar views for Category, MenuItem, and Modifier
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrEmployee]
+
+    def perform_create(self, serializer):
+        menu = Menu.objects.get(pk=self.request.data['menu'])
+        if self.request.user.is_owner or self.request.user.is_employee and self.request.user.restaurant == menu.restaurant:
+            serializer.save()
+        else:
+            raise permissions.PermissionDenied
+
+
+class MenuItemListCreateView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrEmployee]
+
+    def perform_create(self, serializer):
+        category = Category.objects.get(pk=self.request.data['category'])
+        if self.request.user.is_owner or self.request.user.is_employee and self.request.user.restaurant == category.menu.restaurant:
+            serializer.save()
+        else:
+            raise permissions.PermissionDenied
+
+
+class ModifierListCreateView(generics.ListCreateAPIView):
+    queryset = Modifier.objects.all()
+    serializer_class = ModifierSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrEmployee]
+
+    def perform_create(self, serializer):
+        menu_item = MenuItem.objects.get(pk=self.request.data['menu_item'])
+        if self.request.user.is_owner or self.request.user.is_employee and self.request.user.restaurant == menu_item.category.menu.restaurant:
+            serializer.save()
+        else:
+            raise permissions.PermissionDenied
